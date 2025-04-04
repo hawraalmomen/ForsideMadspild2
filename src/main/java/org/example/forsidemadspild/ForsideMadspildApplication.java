c387f9126cf9ab1e228fa8ab2e050b6be02258bd
Hawra Al-Momen <haal0006@stud.kea.dk>
2025-04-04 09:49:53 +0200

    Initial commit

---
+++ src/main/java/org/example/forsidemadspild/ForsideMadspildApplication.java
@@ -0,0 +1,13 @@
+package org.example.forsidemadspild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForsideMadspildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForsideMadspildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/forsidemadspild/ForsideMadspildApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.forsidemadspild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForsideMadspildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
